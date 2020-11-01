0d340d2bf77594f43dadcf73d1d37efc64337dd7
wanghong <931912035@qq.com>
2020-11-01 13:06:33 +0800

    home与search之间的传参，优化性能

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
     <Header></Header>
-    
+
     <router-view></router-view>
     <Footer v-show="!$route.meta.isHidden"></Footer>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     Header,
-    Footer
-  }
-}
+    Footer,
+  },
+  mounted() {
+    this.getCategoryList();
+  },
+  methods: {
+    getCategoryList() {
+      this.$store.dispatch("getCategoryList");
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>Search d df <br><br>
-  params参数---{{$route.params.keyword}}
-  <br>
-  <br>
-  params参数: {{keyword}} ------ query参数：{{keyword1}}
+  <div>
+    <TypeNav></TypeNav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Search',
-  props:['keyword','keyword1']
-}
+  name: "Search",
+  props: ["keyword", "keyword1"],
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
