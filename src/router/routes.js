8b411d0e0c41c6cfa7b963ad947f0743a46a3997
wanghong521 <931912035@qq.com>
2020-11-23 20:31:37 +0800

    完善项目

--- src/router/routes.js
+++ src/router/routes.js
@@ -133,5 +133,53 @@ export default [
   {
     path:'/',
     redirect:'/home'
-  }
+  },
+  {
+    path: '/communication',
+    component: () => import('@/views/Communication/Communication'),
+    children: [
+      {
+        path: 'event',
+        component: () => import('@/views/Communication/EventTest/EventTest'),
+        meta: {
+          isHideFooter: true
+        },
+      },
+      {
+        path: 'model',
+        component: () => import('@/views/Communication/ModelTest/ModelTest'),
+        meta: {
+          isHideFooter: true
+        },
+      },
+      {
+        path: 'sync',
+        component: () => import('@/views/Communication/SyncTest/SyncTest'),
+        meta: {
+          isHideFooter: true
+        },
+      },
+      {
+        path: 'attrs-listeners',
+        component: () => import('@/views/Communication/AttrsListenersTest/AttrsListenersTest'),
+        meta: {
+          isHideFooter: true
+        },
+      },
+      {
+        path: 'children-parent',
+        component: () => import('@/views/Communication/ChildrenParentTest/ChildrenParentTest'),
+        meta: {
+          isHideFooter: true
+        },
+      },
+      {
+        path: 'scope-slot',
+        component: () => import('@/views/Communication/ScopeSlotTest/ScopeSlotTest'),
+        meta: {
+          isHideFooter: true
+        },
+      }
+    ],
+  },
 ]
